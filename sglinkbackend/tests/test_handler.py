51e2d09d1a80880a0345e5d6e2ff2d3268d13a78
sgaskil6 <helpinmyself2oo@gmail.com>
2023-02-27 23:17:35 -0500

    again

--- sglinkbackend/tests/test_handler.py
+++ sglinkbackend/tests/test_handler.py
@@ -3,7 +3,7 @@ import re
 import json
 from unittest import mock
 
-from sglink import app
+from function import app
 
 with open('resume/template.yaml', 'r') as f:
     TABLENAME = re.search(r'TableName: (.*)?', f.read()).group(1)
