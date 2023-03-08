9f9e1d2550a860efb517d47c7a3402525778d7c9
sgaskil6 <helpinmyself2oo@gmail.com>
2023-03-08 00:32:52 -0500

    fixed boto3 region

--- sglinkbackend/sglink/app.py
+++ sglinkbackend/sglink/app.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('visitCounter')
 
 def handler(event, context):
